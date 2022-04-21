df033b21336045858b83a6e07422692872597cca
Hanna Jantunen <hannajant@gmail.com>
2022-04-21 20:57:25 +0300

    fix lint errors

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,29 +3,36 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1})
-    response.json(users.map(u => u.toJSON()))
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  })
+  response.json(users.map((u) => u.toJSON()))
 })
 
 usersRouter.post('/', async (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    const saltRounds = 10
+  const saltRounds = 10
 
-    if(body.password && body.password.length > 2) {
-        const passwordHash = await bcrypt.hash(body.password, saltRounds)
-        const user = new User({
-            username: body.username,
-            name: body.name,
-            passwordHash,
-        })
-        
-        const savedUser = await user.save()
-    
-        response.json(savedUser)
-    } else {
-        return response.status(400).json({ error: 'password missing or too short (min length 3)' })
-    }
+  if (body.password && body.password.length > 2) {
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+  } else {
+    return response
+      .status(400)
+      .json({ error: 'password missing or too short (min length 3)' })
+  }
 })
 
 module.exports = usersRouter
