69ff3f7adfbe8a28160c48a91782932b2d324282
Hanna Jantunen <hannajant@gmail.com>
2022-04-25 11:15:01 +0300

    fix tests

--- client/components/BlogForm.test.js
+++ client/components/BlogForm.test.js
@@ -7,8 +7,9 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
 import BlogForm from './BlogForm'
+import {act} from 'react-dom/test-utils'
 
-test('<BlogForm /> sends out correct information when submitted', () => {
+test('<BlogForm /> sends out correct information when submitted', async () => {
   const createBlog = jest.fn()
 
   render(<BlogForm createBlog={createBlog} />)
@@ -18,11 +19,11 @@ test('<BlogForm /> sends out correct information when submitted', () => {
   const urlInput = screen.getByPlaceholderText('write url here')
   const button = screen.getByText('create')
 
-  userEvent.type(titleInput, 'testing title')
-  userEvent.type(authorInput, 'testing author')
-  userEvent.type(urlInput, 'www.test.fi')
-  userEvent.click(button)
-
+  await act(() => userEvent.type(titleInput, 'testing title'))
+  await act(() => userEvent.type(authorInput, 'testing author')) 
+  await act(() => userEvent.type(urlInput, 'www.test.fi'))  
+  await act(() => userEvent.click(button))  
+  
   expect(createBlog.mock.calls).toHaveLength(1)
   expect(createBlog.mock.calls[0][0].title).toBe('testing title')
   expect(createBlog.mock.calls[0][0].author).toBe('testing author')
